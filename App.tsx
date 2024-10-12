f10fea44d960bec3b7ef54a21bb6601af7e697d3
200~SoraPhong43 <vanphong1010ds@gmail.com>
2024-10-12 17:40:39 +0700

    video29

--- App.tsx
+++ App.tsx
@@ -1,9 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+
 export default function App() {
+  const [name, setName] = useState<string>("Phong");
+
+  //number
+  const [age, setAge] = useState<number>(21)
+
+  //null, undefined, boolean
+  const test = null;
+
+  //object, array
+  const [person, setPerson] = useState([6, 9])
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>{name}</Text>
+        <Text style={styles.text}>{JSON.stringify(person)}</Text>
+      </View>
       <Text style={styles.text}>Hoc lai</Text>
       <StatusBar style="auto" />
     </View>
