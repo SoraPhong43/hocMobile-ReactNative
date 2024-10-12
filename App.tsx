ad0c2cd06100ff8cf81ce4ec85a3fc970205cf8a
200~SoraPhong43 <vanphong1010ds@gmail.com>
2024-10-12 21:15:17 +0700

    video 31

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 
 
 export default function App() {
   const [name, setName] = useState<string>("Phong");
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "learn React Native" },
+    { id: 2, title: "learn React " },
+    { id: 3, title: "learn React JS" },
+    { id: 4, title: "learn React 1" },
+    { id: 5, title: "learn React 2" },
+    { id: 6, title: "learn React 3" },
+    { id: 7, title: "learn React 4" },
+    { id: 8, title: "learn React 5" },
+  ])
+
 
   return (
     <View style={styles.container}>
@@ -28,8 +40,15 @@ export default function App() {
         <Text style={styles.text}>{name}</Text>
       </View>
 
-      <Button title='add' />
-      <Text style={styles.text}>Hoc lai</Text>
+      <Button title='add' color={"green"}
+        onPress={() => alert("tap")} />
+      <View style={{ marginTop: 20, borderColor: 'red', borderWidth: 1 }}>
+        {todoList.map(todo => {
+          return (
+            <Text style={styles.todo}>{todo.title}</Text>
+          )
+        })}
+      </View>
       {/* <StatusBar style="auto" /> */}
     </View>
   );
@@ -43,8 +62,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 10
+    fontSize: 60,
+    color: 'red',
+    paddingHorizontal: 20,
+    marginTop: 50
     //alignItems: 'center',
     // justifyContent: 'center',
   },
+  todo: {
+    fontSize: 30,
+    backgroundColor: 'pink',
+    marginBottom: 20,
+    padding: 15
+  }
 });
